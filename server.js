50fe0b9801bba8e21893103b2e76ffdf352039bb
Callan Morrison <callan.l.morrison@gmail.com>
2017-09-06 08:59:35 -0500

    give cats special skillz

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ const gifSearch = require ('gif-search');
 const catNames = require('cat-names');
 
 
-const specialSkillz = ["Hardly sounds like he’s from Boston at all.", "Willing to be big spoon or little spoon.", "Has a terrific family recipe for brownies.", "Used to be able to do “the worm” kind of.", "Very reliable for giving rides to/ from the airport."]
-
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -25,6 +23,9 @@ app.use(function(req, res, next) {
 
 
 const colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Black", "Brown", "Grey", "Poop-Colored"];
+
+const specialSkillz = ["Expertly quotes and recognizes dialogue from early seasons of The Simpsons.","Can be around boobs without staring creepily at them.","Proficient in air guitar","Able to lift slightly more than you’d think.","Gives sincere apologies.","Owns a Nintendo Power Glove.","Not too shabby at spelling.","Will not make a scene when your grandmother says something mildly anti-Semitic.","Gives thoughtful and creative birthday presents.","Adept at talking self and others out of fights.","Takes the morning shift driving on road trips.","Admits he is wrong","Is not racist.","Talks about sports with enthusiasm and accuracy.","Sits back seat middle if necessary.","Participates in karaoke but does not force others to go out to karaoke.","Dances when he has to.","Does not freak out if you haven’t seen his favorite movie (The Big Lebowski).","Will loan you books without freaking out if you have them for a while.","Can read (but cannot understand) Hebrew.","Is totally laid back if you spell his name wrong.","Gives compliments when compliments are due.","Able to stop chewing ice or whistling on request.","Just picks the tomatoes off of a sandwich instead of requesting a whole new sandwich.","Drives at a safe rate of speed in snow or rain.","Knows who Michel Foucault is.","Doesn’t get weirded out by the word “moist.”","Comfortable in the outdoors for up to eight hours.","Can prove he is a real man by drinking whiskey.","Gives hugs with appropriate pressure and for the right length of time.","Showers regularly.","Is comfortable with jokes about his receding hairline.","Will pet your cat even though he does not like your cat.","Exhibits eagerness to visit any non-Holocaust museum.","Burps minimally.","Listens attentively to boring stories.","Always up for dessert."]
+
 
 function randomIntFromInterval(min,max)
 {
@@ -79,7 +80,7 @@ app.get('/cats/*', (req, res) => {
       color: colors[randomIntFromInterval(0, colors.length - 1)],
     }
 
-    newCat["specialSkill"] = [getRandomPrice(0, 7000)];
+    newCat["specialSkill"] = specialSkillz[randomIntFromInterval(0, specialSkillz.length - 1)];
     newCat["numberOfToes"] = randomIntFromInterval(0,20)
 
 
